e4aaf995ca10b9b0d6064b476c1c698f84a4d43b
judiiiiii_gp <judit.garcia.pallares@estudiantat.upc.edu>
2024-10-08 13:13:41 +0200

    He començat a afegir les classes per a cada Data Item

--- Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0-windows/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075b6fe4bf7d805674f564cbccfa9510665dff69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
